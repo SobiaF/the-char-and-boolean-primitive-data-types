1a4c1eed89a98bdfa0a91da975896ab93efc5452
SobiaF <25015478+SobiaF@users.noreply.github.com>
2020-12-02 12:44:44 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package learningjava;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'S';
+        // Can only store a single character in a single char variable.
+        char myUnicodeChar = '\u0053';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+        boolean myTueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverFifteen = true;
+    }
+}
